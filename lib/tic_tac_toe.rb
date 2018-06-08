524a1561ac3091ed9f46b016a294e12d2476b6ce
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-08 03:57:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,7 +22,7 @@ class TicTacToe
     display_row(2)
   end
 
-  def display_row(board, row_index)
+  def display_row(row_index)
     puts " #{@board[3 * row_index]} | #{@board[3 * row_index + 1]} | #{@board[3 * row_index + 2]} "
   end
 end
