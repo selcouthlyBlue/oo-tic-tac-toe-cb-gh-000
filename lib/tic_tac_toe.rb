cbf686f1eebaad40dbc650e580fc1d2a1c279fea
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-08 03:53:07 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,5 @@
 class TicTacToe
-  WIN_COMBINATIONS = {
+  WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -8,7 +8,7 @@ class TicTacToe
     [2,5,8],
     [0,4,8],
     [2,4,6]
-  }
+  ]
 
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
